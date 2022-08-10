bfab2139840d3b56a2e906352f8d04a014e3677e
Tsering Tsering <dtsering2@gmail.com>
2022-08-10 12:29:47 -0400

    done with assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+export default function EyesOnMe(){
+    function focusHandler(){
+        console.log("Good!")
+    }
+    function blurHandler(){
+        console.log("Hey! Eyes on me!")
+    }
+    return(
+        <button onFocus = {focusHandler} onBlur = {blurHandler}>Eyes on me</button>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
-
+function someHandler (e){
+    e.preventDefault()
+    console.log(`Entering password...`)
+}
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <form>
+                <input type = "password" onChange = {someHandler} ></input>
+            </form>
+        </div>
     )
 }
 
